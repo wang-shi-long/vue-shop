c06d384a196a81022a53b3015fe32d1153e50459
澄城二级 <5646567+wsl_bnzw@user.noreply.gitee.com>
2020-12-13 16:41:58 +0800

    add files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <index>
+      <template v-slot="$props">
+        {{ $props }}
+      </template>
+    </index>
+  </div>
+</template>
+
+<script>
+import index from "../pages/index";
+export default {
+  components: {
+    index,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/utils/element.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import elementUI from 'element-ui'
+Vue.use(elementUI)
+import 'element-ui/lib/theme-chalk/index.css'
